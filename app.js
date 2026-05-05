7077b1dda60252dd13fc63bf4bdc221429a59781
taniabillordo-byte <tania.billordo@cablasociados.com>
2026-05-05 03:42:04 +0000

    Agrego un nuevo endpoint

--- app.js
+++ app.js
@@ -10,5 +10,11 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "API Funcionando" });
 });
 
+// Define 
+app.get("/", (req, res) => {
+    // Envía una respuesta con estado 200 (OK) y un objeto JSON con el mensaje
+    res.status(200).json({ message: "Ok", clients: [C1, C2] });
+});
+
 // Exporta la instancia de 'app' para que pueda ser utilizada en otros archivos (como server.js)
 module.exports = app;
